cff554b2769eb235633633086b53969ba935216c
SystemsPurge <naktiyoussef@proton.me>
2024-11-07 15:57:27 +0100

    Adjusting provision of sim start parameters
    Signed-off-by: SystemsPurge <naktiyoussef@proton.me>

--- villas/controller/components/simulators/dpsim.py
+++ villas/controller/components/simulators/dpsim.py
@@ -23,9 +23,39 @@ class DPsimSimulator(Simulator):
     def load_cim(self, fp):
         if fp is not None:
             reader = dpsimpy.CIMReader(fp)
-            self.sim = reader.loadCIM(50, list([fp]), dpsimpy.Domain.SP, dpsimpy.PhaseType.Single, dpsimpy.GeneratorType.PVNode)
+            files = list(map(lambda x: f'{fp}/{x}',os.listdir(fp)))
+            self.logger.info(files)
+            freq = self.params.get("system-freq",50)
+            duration = self.params.get("duration",10)
+            timestep = self.params.get("timestep",1)
+            domain_str = self.params.get("solver-domain","SP")
+            solver_str = self.params.get("solver-type","MNA")
+            
+            if domain_str == "SP":
+                domain = dpsimpy.Domain.SP
+            elif domain_str == "DP":
+                domain = dpsimpy.Domain.DP
+            else :
+                domain = dpsimpy.Domain.EMT
+
+            if solver_str == "MNA":
+                solver = dpsimpy.Solver.MNA
+            else:
+                solver = dpsimpy.Solver.NRP
+            
+            system = reader.loadCIM(freq, files, domain, dpsimpy.PhaseType.Single, dpsimpy.GeneratorType.PVNode) #self.params system-freq
+            self.sim = dpsimpy.Simulation(fp)
+            self.sim.set_system(system)
+            self.sim.set_domain(domain) #self.params solver-domain
+            self.sim.set_solver(solver) #self.params solver-type
+            self.sim.set_time_step(timestep) #self.params timestep
+            self.sim.set_final_time(duration) #self.params duration
+            logger = dpsimpy.Logger(fp)
+            for node in system.nodes:
+                logger.log_attribute(node.name()+'.V', 'v', node)
+            self.sim.add_logger(logger)
             self.logger.info(self.sim)
-            os.unlink(fp.name)
+            os.unlink(fp)
 
     def start(self, payload):
         super().start(payload)
